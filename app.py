1eab17ae01899b48a33f7129f4d2a28492745ccf
Harsith2k5 <harsithharsith217@gmail.com>
2025-08-05 05:57:35 +0530

    Pushing to new repo

--- app.py
+++ app.py
@@ -1354,4 +1354,5 @@ def merge_station_data(duplicates):
 
 # --- Improved Main Route ---
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5501, debug=True)
+    port = int(os.environ.get('PORT', 5501))  # Render provides PORT environment variable
+    app.run(host='0.0.0.0', port=port)
